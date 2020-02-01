349f039a4979a3157b9cba682f298de4334e4a12
Aleksandr Movchan <movchan174@gmail.com>
2020-02-01 11:06:01 +0100

    FPS control.

---
+++ Assets/ObjectsManipulation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsManipulation : MonoBehaviour
+{
+    public Vector3 direction = Vector3.forward;
+    private Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.Find("FirstPersonCharacter").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.DrawRay(ray.origin, ray.direction*1000, Color.yellow);
+                Debug.Log("Did Hit");
+                Debug.Log(hit.collider.gameObject);
+            }
+            else
+            {
+                Debug.DrawRay(ray.origin, ray.direction*1000, Color.white);
+                Debug.Log("Did not Hit");
+            }
+            
+        }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 10f;
+    public float camSens = 0.25f; //How sensitive it with mouse
+    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+    private CharacterController _controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _controller.Move(move * Time.deltaTime * Speed);
+
+        lastMouse = Input.mousePosition - lastMouse ;
+        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0 );
+        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x , transform.eulerAngles.y + lastMouse.y, 0);
+        transform.eulerAngles = lastMouse;
+        lastMouse =  Input.mousePosition;
+    }
+}
